2534897a328e2719d019c350045e68d144f400f9
xueqianLu <xueqian1991@163.com>
2018-11-09 16:06:19 +0800

    revert to last version

--- txpool/tx_pool.go
+++ txpool/tx_pool.go
@@ -177,7 +177,7 @@ func (pool *TxPool) Stop() {
 	}
 }
 
-func boecallback(rs boe.RecoverResult,err error) {
+func boecallback(rs boe.RecoverPubkey,err error) {
 	if err != nil {
 		log.Trace("boe validatesign error")
 	}
